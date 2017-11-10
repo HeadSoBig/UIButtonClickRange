1f8ade00871f7c1c22c243e4c005ad33ec1be855
cnliveJunBo <wangjunbo@cnlive.com>
2017-11-10 10:23:22 +0800

    新建项目

---
+++ UIButtonClickRangeDemo/UIButtonClickRangeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIButtonClickRangeDemo
+//
+//  Created by cnliveJunBo on 2017/11/10.
+//  Copyright © 2017年 cnlive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIButtonClickRangeDemo/UIButtonClickRangeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIButtonClickRangeDemo
+//
+//  Created by cnliveJunBo on 2017/11/10.
+//  Copyright © 2017年 cnlive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
